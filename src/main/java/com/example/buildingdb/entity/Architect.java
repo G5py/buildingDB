7d4a9bcddf87408a0ab835ba48e94850cb676581
GSpy <55821668+G5py@users.noreply.github.com>
2025-07-25 08:55:44 +0900

    feat : setter 추가.

--- src/main/java/com/example/buildingdb/entity/Architect.java
+++ src/main/java/com/example/buildingdb/entity/Architect.java
@@ -2,10 +2,12 @@ package com.example.buildingdb.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "ARCHITECTS")
 @Getter
+@Setter
 public class Architect {
 
     @Id
