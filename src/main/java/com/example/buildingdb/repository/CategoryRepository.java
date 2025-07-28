4a9aedb42f673c3879686cd4bb2109d16ea94e52
GSpy <55821668+G5py@users.noreply.github.com>
2025-07-28 20:57:00 +0900

    feat : findByTag 메소드 추가.

--- src/main/java/com/example/buildingdb/repository/CategoryRepository.java
+++ src/main/java/com/example/buildingdb/repository/CategoryRepository.java
@@ -1,7 +1,13 @@
 package com.example.buildingdb.repository;
 
+import com.example.buildingdb.entity.Building;
 import com.example.buildingdb.entity.Category;
+import com.example.buildingdb.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    public List<Building> findByTag(Tag tag);
 }
