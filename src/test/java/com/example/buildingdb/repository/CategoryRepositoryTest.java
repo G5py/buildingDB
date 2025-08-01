a92c2de1419d8442d933d79085448adc1d9030b9
GSpy <55821668+G5py@users.noreply.github.com>
2025-08-01 11:40:37 +0900

    fix : 불필요한 의존성 삭제.

--- src/test/java/com/example/buildingdb/repository/CategoryRepositoryTest.java
+++ src/test/java/com/example/buildingdb/repository/CategoryRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.example.buildingdb.repository;
 
 
-import com.example.buildingdb.entity.Architect;
 import com.example.buildingdb.entity.Building;
 import com.example.buildingdb.entity.Category;
 import com.example.buildingdb.entity.Tag;
