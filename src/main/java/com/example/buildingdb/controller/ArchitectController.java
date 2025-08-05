4321a47dba08a698b89f4633e589841bf96efdd4
GSpy <55821668+G5py@users.noreply.github.com>
2025-08-05 23:30:38 +0900

    feat : ArchitectController의 CRUD 구현.

--- src/main/java/com/example/buildingdb/controller/ArchitectController.java
+++ src/main/java/com/example/buildingdb/controller/ArchitectController.java
@@ -5,30 +5,37 @@ import com.example.buildingdb.dto.ArchitectDto;
 import com.example.buildingdb.exception.InvalidDataException;
 import com.example.buildingdb.service.ArchitectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/architects")
 public class ArchitectController {
 
-    private ArchitectService architectService;
+    private final ArchitectService architectService;
 
     @Autowired
     public ArchitectController(ArchitectService architectService) {
         this.architectService = architectService;
     }
 
-    @GetMapping("/{id}")
-    public ArchitectDto getArchitect(@PathVariable Long id) throws InvalidDataException{
-        try {
-            return architectService.getArchitect(id);
-        } catch (InvalidDataException e) {
-            throw new InvalidDataException(e.getMessage());
-        }
-
+    @PostMapping
+    public ArchitectDto postArchitect(@RequestBody ArchitectDto architectDto) throws InvalidDataException {
+        return architectService.addArchitect(architectDto);
     }
 
+
+    @GetMapping("/{id}")
+    public ArchitectDto getArchitect(@PathVariable Long id) throws InvalidDataException {
+        return architectService.getArchitect(id);
+    }
+
+    @PutMapping
+    public void putArchitect(@RequestBody ArchitectDto architectDto) throws InvalidDataException {
+        architectService.putArchitect(architectDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteArchitect(@PathVariable Long id) throws InvalidDataException {
+        architectService.deleteArchitect(id);
+    }
 }
