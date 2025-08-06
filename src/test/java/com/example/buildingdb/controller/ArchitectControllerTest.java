4ae558dd3c6bc1b5578f84c6c3ee9388c00bfe7e
GSpy <55821668+G5py@users.noreply.github.com>
2025-08-07 01:27:58 +0900

    test : ArchitectController 클래스의 테스트 코드 추가.

---
+++ src/test/java/com/example/buildingdb/controller/ArchitectControllerTest.java
@@ -0,0 +1,102 @@
+package com.example.buildingdb.controller;
+
+import com.example.buildingdb.dto.ArchitectDto;
+import com.example.buildingdb.exception.InvalidDataException;
+import com.example.buildingdb.service.ArchitectService;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ArchitectControllerTest {
+
+    @Test
+    void testPostArchitect() {
+        try {
+            // given
+            ArchitectDto dto = new ArchitectDto(1L, "Tadao Ando", "안도 다다오");
+
+            ArchitectService mockService = mock(ArchitectService.class);
+            when(mockService.addArchitect(dto))
+                    .thenReturn(dto);
+
+            ArchitectController controller = new ArchitectController(mockService);
+
+            // when
+            ArchitectDto actual = controller.postArchitect(dto);
+
+            // then
+            assertThat(actual).isEqualTo(dto);
+        } catch (InvalidDataException e) {
+            fail("Exception occurred.");
+        }
+    }
+
+    @Test
+    void testGetArchitect() {
+        try {
+            // given
+            ArchitectDto expected = new ArchitectDto(1L, "Tadao Ando", "안도 다다오");
+
+            ArchitectService mockService = mock(ArchitectService.class);
+            when(mockService.getArchitect(1L))
+                    .thenReturn(expected);
+
+            ArchitectController controller = new ArchitectController(mockService);
+
+            // when
+            ArchitectDto actual = controller.getArchitect(1L);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        } catch (InvalidDataException e) {
+            fail("Exception occurred.");
+        }
+    }
+
+    @Test
+    void testPutArchitect() {
+        try {
+            // given
+            ArchitectDto dto = new ArchitectDto(1L, "Tadao Ando", "안도 다다오");
+
+            ArchitectService mockService = mock(ArchitectService.class);
+            when(mockService.putArchitect(dto))
+                    .thenReturn(dto);
+
+            ArchitectController controller = new ArchitectController(mockService);
+
+            // when
+            ArchitectDto actual = controller.putArchitect(dto);
+
+            // then
+            assertThat(actual).isEqualTo(dto);
+        } catch (InvalidDataException e) {
+            fail("Exception occurred.");
+        }
+    }
+
+    @Test
+    void testDeleteArchitect() {
+        try {
+            // given
+            ArchitectDto expected = ArchitectDto.builder()
+                    .id(1L)
+                    .build();
+
+            ArchitectService mockService = mock(ArchitectService.class);
+
+            ArchitectController controller = new ArchitectController(mockService);
+
+            // when
+            ArchitectDto actual = controller.deleteArchitect(1L);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        } catch (InvalidDataException e) {
+            fail("Exception occurred.");
+        }
+    }
+}
