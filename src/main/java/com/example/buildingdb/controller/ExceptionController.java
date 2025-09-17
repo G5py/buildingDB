5aad7e312d665ea9da852ea4542e33b87dafd745
GSpy <55821668+G5py@users.noreply.github.com>
2025-09-17 13:37:57 +0900

    feat : EntityNotFoundException 예외를 처리하는 구문 추가.

--- src/main/java/com/example/buildingdb/controller/ExceptionController.java
+++ src/main/java/com/example/buildingdb/controller/ExceptionController.java
@@ -3,6 +3,7 @@ package com.example.buildingdb.controller;
 
 import com.example.buildingdb.dto.ErrorResponse;
 import com.example.buildingdb.exception.InvalidDataException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,7 +14,7 @@ import java.net.URISyntaxException;
 public class ExceptionController {
 
     @ExceptionHandler(InvalidDataException.class)
-    public ResponseEntity<ErrorResponse> HandleInvalidDataException(InvalidDataException e) {
+    public ResponseEntity<ErrorResponse> handleInvalidDataException(InvalidDataException e) {
         return ResponseEntity
                 .badRequest()
                 .body(new ErrorResponse(e.getMessage()));
@@ -24,5 +25,12 @@ public class ExceptionController {
         return ResponseEntity
                 .internalServerError()
                 .body("Failed to create URI.");
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse(e.getMessage()));
     }
 }
